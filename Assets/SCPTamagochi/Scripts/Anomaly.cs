81c4aa5e125f34c2c7072f8eec3ca3a618a1f39f
Skavarada <Skavarada@WIN-0U181J1AE2C>
2022-11-15 15:02:15 +0700

    Add Tag class

--- Assets/SCPTamagochi/Scripts/Anomaly.cs
+++ Assets/SCPTamagochi/Scripts/Anomaly.cs
@@ -5,19 +5,26 @@ using UnityEngine.UI;
 
 public class Anomaly : MonoBehaviour
 {
+    Anomaly.FoodTag fruitTag = new Anomaly.FoodTag("Фрукты", Anomaly.FOOD.FRUIT);
+    Anomaly.FoodTag meatTag = new Anomaly.FoodTag("Мясо", Anomaly.FOOD.MEAT);
+
     public enum FOOD {MEAT, FRUIT};
 
     [SerializeField] private Text Info;
     private SpriteRenderer sr;
     private FOOD _currentFood;
     private FOOD _preferFood;
+    private List<Tag> tags;
 
     public FOOD Food { get { return _currentFood; } set { _currentFood = value; } }
 
     private void Awake()
     {
         sr = gameObject.GetComponent<SpriteRenderer>();
-        _preferFood = (FOOD)Random.Range(0, 2);
+        //_preferFood = (FOOD)Random.Range(0, 2);
+        tags = new List<Tag>(1);
+        tags.Add(meatTag);
+        tags[0].SettingTag(this);
         InfoUpdate();
     }
 
@@ -43,7 +50,38 @@ public class Anomaly : MonoBehaviour
     public void InfoUpdate()
     {
         Info.text = "Рацион: " + _currentFood.ToString();
+        Info.text += "\nТеги: ";
+        for(int i = 0; i < tags.Count; i++)
+        {
+            Info.text += tags[i].Name;
+        }
     }
 
-    
+    public class Tag
+    {
+        public Tag(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; protected set; }
+        public delegate void TagDelegate(Anomaly anomaly);
+        public TagDelegate SettingTag;
+    }
+
+    public class FoodTag : Anomaly.Tag
+    {
+        public FoodTag(string name, Anomaly.FOOD preferFood) : base(name)
+        {
+            Name = name;
+            _preferFood = preferFood;
+            SettingTag = SetFood;
+        }
+
+        private Anomaly.FOOD _preferFood;
+
+        public void SetFood(Anomaly anomaly)
+        {
+            anomaly._preferFood = _preferFood;
+        }
+    }
 }
